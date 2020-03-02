84c02d2ec5a2e9f29c34308490fe33107720e288
Eduardo Donato <zoroah@Outlook.com>
2020-03-02 08:33:04 -0300

    .

---
+++ precos.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cine São Carlos: Preços</title>
+    <link rel="stylesheet" href="bootstrap.min.css" />
+    <link rel="stylesheet" href="estilos.css" />
+</head>
+
+<body>
+    <div class="container d-flex justify-content-center">
+        <div class="row">
+            <?php require('cabecalho.html');?>
+            <main class="card d-flex justify-content-center">
+                <div class="card-header">
+                    <h1>Preços</h1>
+                </div>
+                <div class="card-body">
+                    <div>
+                        <img src="https://1.bp.blogspot.com/-ao_JvXKPf8E/XfJhuqt-CfI/AAAAAAAAPbM/oPz8hSjbRkQbQXVykno6DGVBHqT3Vo8rACLcBGAsYHQ/s640/0001.jpg"
+                             class="img-fluid">
+                    </div>
+                    <div>
+                        <img src="https://3.bp.blogspot.com/-vyJmx2cbyW0/WgOSlpB_YKI/AAAAAAAAGS8/YGvsug7uFj8PLA4Bz2WsBhEI4a8ONe3jACLcBGAs/s1600/baner%2Bmeia%2Bentrada.png"
+                             class="img-fluid">
+                    </div>
+                </div>
+            </main>
+        </div>
+    </div>
+</body>
+
+</html>   
+        <script type="text/javascript" src="jquery-3.4.1.min.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+        <script type="text/javascript" src="bootstrap.min.js"></script>
